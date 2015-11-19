a9f1acf5f996a07fdcc90f5f7a0a0f7027d5c896
Andreas Lind <andreas@one.com>
2015-11-20 00:32:33 +0100

    Pass indexes to equal and similar.
    Include actualIndex and expectedIndex in diff items where applicable.
    
    Fix processing of array-like objects.

--- test/arrayChanges.spec.js
+++ test/arrayChanges.spec.js
@@ -1,6 +1,7 @@
 /*global describe, it*/
 var arrayChanges = require('../lib/arrayChanges');
-var expect = require('unexpected');
+var expect = require('unexpected').clone().use(require('unexpected-sinon'));
+var sinon = require('sinon');
 
 function toArguments() {
     return arguments;
@@ -17,10 +18,10 @@ describe('array-changes', function () {
         expect(arrayChanges([0, 1, 2, 3], [0, 1, 2, 3], function (a, b) {
             return a === b;
         }), 'to equal', [
-            { type: 'equal', value: 0, expected: 0 },
-            { type: 'equal', value: 1, expected: 1 },
-            { type: 'equal', value: 2, expected: 2 },
-            { type: 'equal', value: 3, expected: 3, last: true }
+            { type: 'equal', value: 0, expected: 0, actualIndex: 0, expectedIndex: 0 },
+            { type: 'equal', value: 1, expected: 1, actualIndex: 1, expectedIndex: 1 },
+            { type: 'equal', value: 2, expected: 2, actualIndex: 2, expectedIndex: 2 },
+            { type: 'equal', value: 3, expected: 3, actualIndex: 3, expectedIndex: 3, last: true }
         ]);
     });
 
@@ -28,10 +29,10 @@ describe('array-changes', function () {
         expect(arrayChanges([0, 1, 2, 3], [0, 1, 3], function (a, b) {
             return a === b;
         }), 'to equal', [
-            { type: 'equal', value: 0, expected: 0 },
-            { type: 'equal', value: 1, expected: 1 },
-            { type: 'remove', value: 2 },
-            { type: 'equal', value: 3, expected: 3, last: true }
+            { type: 'equal', value: 0, expected: 0, actualIndex: 0, expectedIndex: 0 },
+            { type: 'equal', value: 1, expected: 1, actualIndex: 1, expectedIndex: 1 },
+            { type: 'remove', value: 2, actualIndex: 2 },
+            { type: 'equal', value: 3, expected: 3, actualIndex: 3, expectedIndex: 2, last: true }
         ]);
     });
 
@@ -39,7 +40,7 @@ describe('array-changes', function () {
         expect(arrayChanges([0], [], function (a, b) {
             return a === b;
         }), 'to equal', [
-            { type: 'remove', value: 0, last: true }
+            { type: 'remove', value: 0, actualIndex: 0, last: true }
         ]);
     });
 
@@ -47,10 +48,10 @@ describe('array-changes', function () {
         expect(arrayChanges([0, 1, 3], [0, 1, 2, 3], function (a, b) {
             return a === b;
         }), 'to equal', [
-            { type: 'equal', value: 0, expected: 0 },
-            { type: 'equal', value: 1, expected: 1 },
-            { type: 'insert', value: 2 },
-            { type: 'equal', value: 3, last: true, expected: 3 }
+            { type: 'equal', value: 0, expected: 0, actualIndex: 0, expectedIndex: 0 },
+            { type: 'equal', value: 1, expected: 1, actualIndex: 1, expectedIndex: 1 },
+            { type: 'insert', value: 2, expectedIndex: 2 },
+            { type: 'equal', value: 3, last: true, expected: 3, actualIndex: 2, expectedIndex: 3 }
         ]);
     });
 
@@ -58,7 +59,7 @@ describe('array-changes', function () {
         expect(arrayChanges([], [0], function (a, b) {
             return a === b;
         }), 'to equal', [
-            { type: 'insert', value: 0, last: true }
+            { type: 'insert', value: 0, expectedIndex: 0, last: true }
         ]);
     });
 
@@ -66,11 +67,11 @@ describe('array-changes', function () {
         expect(arrayChanges([1, 2, 3, 0], [0, 1, 2, 3], function (a, b) {
             return a === b;
         }), 'to equal', [
-            { type: 'insert', value: 0, last: false },
-            { type: 'equal', value: 1, expected: 1 },
-            { type: 'equal', value: 2, expected: 2 },
-            { type: 'equal', value: 3, expected: 3 },
-            { type: 'remove', value: 0, last: true }
+            { type: 'insert', value: 0, last: false, actualIndex: 3 },
+            { type: 'equal', value: 1, expected: 1, actualIndex: 0, expectedIndex: 1 },
+            { type: 'equal', value: 2, expected: 2, actualIndex: 1, expectedIndex: 2 },
+            { type: 'equal', value: 3, expected: 3, actualIndex: 2, expectedIndex: 3 },
+            { type: 'remove', value: 0, last: true, actualIndex: 3 }
         ]);
     });
 
@@ -78,10 +79,10 @@ describe('array-changes', function () {
         expect(arrayChanges([0, 1, 2, 3], [0, 2, 1, 3], function (a, b) {
             return a === b;
         }), 'to equal', [
-            { type: 'equal', value: 0, expected: 0 },
-            { type: 'similar', value: 1, expected: 2 },
-            { type: 'similar', value: 2, expected: 1 },
-            { type: 'equal', value: 3, expected: 3, last: true }
+            { type: 'equal', value: 0, expected: 0, actualIndex: 0, expectedIndex: 0 },
+            { type: 'similar', value: 1, expected: 2, actualIndex: 1, expectedIndex: 1 },
+            { type: 'similar', value: 2, expected: 1, actualIndex: 2, expectedIndex: 2 },
+            { type: 'equal', value: 3, expected: 3, actualIndex: 3, expectedIndex: 3, last: true }
         ]);
     });
 
@@ -107,44 +108,93 @@ describe('array-changes', function () {
             {
                 type: 'equal',
                 value: { type: 'dog', name: 'Fido' },
-                expected: { type: 'dog', name: 'Fido' }
+                expected: { type: 'dog', name: 'Fido' },
+                actualIndex: 0,
+                expectedIndex: 0
             },
             {
                 type: 'equal',
                 value: { type: 'dog', name: 'Teddy' },
-                expected: { type: 'dog', name: 'Teddy' }
+                expected: { type: 'dog', name: 'Teddy' },
+                actualIndex: 1,
+                expectedIndex: 1
             },
             {
                 type: 'insert',
-                value: { type: 'dog', name: 'Murphy' }
+                value: { type: 'dog', name: 'Murphy' },
+                expectedIndex: 2
             },
             {
                 type: 'similar',
                 value: { type: 'person', name: 'Sune' },
-                expected: { type: 'person', name: 'Andreas' }
+                expected: { type: 'person', name: 'Andreas' },
+                actualIndex: 2,
+                expectedIndex: 3
             },
             {
                 type: 'equal',
                 value: { type: 'dog', name: 'Charlie' },
-                expected: { type: 'dog', name: 'Charlie' }
+                expected: { type: 'dog', name: 'Charlie' },
+                actualIndex: 3,
+                expectedIndex: 4
             },
             {
                 type: 'equal',
                 value: { type: 'dog', name: 'Sam' },
                 last: true,
-                expected: { type: 'dog', name: 'Sam' }
+                expected: { type: 'dog', name: 'Sam' },
+                actualIndex: 4,
+                expectedIndex: 5
             }
         ]);
     });
 
     it('supports diffing array-like objects', function () {
-        expect(arrayChanges(toArguments(1, 2, 5), toArguments(3, 4), function (a, b) {
+        expect(arrayChanges(toArguments(1, 2, 5), toArguments(1, 3, 4), function (a, b) {
             return a === b;
         }), 'to equal', [
-            { type: 'insert', value: toArguments( 3, 4 ) },
-            { type: 'remove', value: 1 },
-            { type: 'similar', value: 2, expected: 4 },
-            { type: 'similar', value: 5, last: true, expected: undefined }
+            { type: 'equal', value: 1, expected: 1, actualIndex: 0, expectedIndex: 0 },
+            { type: 'similar', value: 2, expected: 3, actualIndex: 1, expectedIndex: 1 },
+            { type: 'similar', value: 5, expected: 4, actualIndex: 2, expectedIndex: 2, last: true }
+        ]);
+    });
+
+    it('passes the item indices to the equal function', function () {
+        var a = [1, 2];
+        var b = [4, 5];
+        var equal = sinon.spy(function (a, b) {
+            return a === b;
+        }).named('equal');
+        arrayChanges(a, b, equal);
+        expect(equal, 'to have calls satisfying', [
+            { args: [ 1, 4, 0, 0 ] },
+            { args: [ 1, 5, 0, 1 ] },
+            { args: [ 2, 4, 1, 0 ] },
+            { args: [ 2, 5, 1, 1 ] },
+            { args: [ 1, 4, 0, 0 ] },
+            { args: [ 2, 5, 1, 1 ] },
+            { args: [ 1, 4, 0, 0 ] },
+            { args: [ 2, 5, 1, 1 ] }
+        ]);
+    });
+
+    it('passes the item indices to the similar function', function () {
+        var a = [1, 2];
+        var b = [4, 5];
+        var equal = sinon.spy(function (a, b) {
+            return a === b;
+        }).named('equal');
+        var similar = sinon.spy(function (a, b) {
+            return a === b;
+        }).named('similar');
+        arrayChanges(a, b, equal, similar);
+        expect(similar, 'to have calls satisfying', [
+            { args: [ 1, 4, 0, 0 ] },
+            { args: [ 1, 5, 0, 1 ] },
+            { args: [ 2, 4, 1, 0 ] },
+            { args: [ 2, 5, 1, 1 ] },
+            { args: [ 1, 4, 0, 0 ] },
+            { args: [ 2, 5, 1, 1 ] }
         ]);
     });
 });
